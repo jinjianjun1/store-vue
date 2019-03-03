a4df0ae3d93085784e9b61c52505fb9ba6c41396
jason <13141374484@163.com>
2019-03-03 15:46:33 +0800

    转单页 商城

---
+++ src/assets/js/api.js
@@ -0,0 +1,30 @@
+let url ={
+    hotLists:'/index/hotLists',
+    banner:'/index/banner',
+    toplist:'/category/topList',
+    ranklist:'/category/rank',
+    sublist:'/category/subList',
+    searchlist:'/search/list',
+    details:'/goods/details',
+    deal:'/goods/deal',
+    add:'/cart/add',
+    cartlist:'/cart/list',
+    reduce:'/cart/reduce',
+    remove:'/cart/remove',
+    meremove:'/cart/mrremove',
+    addressList:'/address/list',
+    addressAdd:'/address/add',
+    addressUpdate:'/address/update',
+    addressRemove:'/address/remove',
+    addressSetDefault:'/address/setDefault'
+}
+//开发环境和真实环境切换
+let host = 'https://easy-mock.com/mock/5c34c54490862b0b0cf503d9/goodlists'
+
+for ( let key in url){
+    if(url.hasOwnProperty(key)){
+        url[key]=host +url[key]
+    }
+}
+
+export default url
---
+++ src/components/swip.vue
@@ -0,0 +1,61 @@
+<template>
+   <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swp-page swiper-slide" v-for="list in lists " :key="list.id">
+            <a class="js-no-follow" :href="list.clickUrl">
+                <img class="goods-main-photo fadeIn" :src="list.img">
+            </a>
+            </div>
+            
+            <!-- <div class="swiper-scrollbar"></div> -->
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper'
+    import 'swiper/dist/css/swiper.css'
+    export default {
+        name:'swiper',
+        props:{
+            lists:{
+                type:Array,
+                required:true
+            },
+            name:{}
+        },
+        created(){
+            console.log(this.lists)
+        },
+        mounted(){
+            // new Swiper('.swiper-container',{
+            //     // direction: 'vertical',
+            //     loop: true,
+            //     pagination: {
+            //         el: '.swiper-pagination',
+            //         type: 'bullets'
+            //     },
+            //     // scrollbar: {
+            //     //     el: '.swiper-scrollbar',
+            //     // },
+            // })
+             var swiper = new Swiper('.swiper-container', {
+                pagination: {
+                el: '.swiper-pagination',
+                },
+                loop: true, 
+                autoplay: {
+                    delay: 3000,
+                },
+            });
+        }
+    }
+</script>
+
+<style scoped>
+.swiper-slide img{
+    width: 100%;
+    height: 100%;
+}
+</style>
