4e6d151baedc29be15b17084936b262a941876e4
jason <13141374484@163.com>
2019-03-03 16:35:36 +0800

    调试生产模式2.1

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,8 @@ function resolve (dir) {
     return path.join(__dirname, dir)
 }
 module.exports = {
+    publicPath: './',
+    // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
-    runtimeCompiler : true,
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/store-vue/'
-        : './',
-    outputDir: 'dist',
 
     }
